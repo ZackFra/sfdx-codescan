61c3599df232a315ddd02ef8bb5db4113f72eb71
Zackary Frazier <zack11frazier22@gmail.com>
2024-12-21 10:37:45 -0800

    Add max severity input to code scan workflow and update warning script

--- scripts/raise-warnings.js
+++ scripts/raise-warnings.js
@@ -1,6 +1,7 @@
 // `::warning file={name},line={line},endLine={endLine},title={title}::{message}`
 
 const fs = require("fs");
+const maxSeverity = parseInt(process.argv[2]);
 
 const codeScanFile = JSON.parse(fs.readFileSync("codescan.json", "utf8"));
 
@@ -20,4 +21,8 @@ errors.sort((a, b) => a.severity - b.severity);
 
 for (const error of errors) {
   console.log(error.message);
+}
+
+if(errors[0].severity <= maxSeverity) {
+  process.exit(1);
 }
