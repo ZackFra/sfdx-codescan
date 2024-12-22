4385f6ed8d4844c5887c07159978f87df2695b03
Zackary Frazier <zack11frazier22@gmail.com>
2024-12-21 21:05:45 -0800

    Refactor move-directories script to use environment variables for action path and workspace

--- .scripts/ts/move-directories.ts
+++ .scripts/ts/move-directories.ts
@@ -1,12 +1,12 @@
 import fs from 'fs'
 
-const workflowDir = process.argv[2]
+const actionPath = process.env.GITHUB_ACTION_PATH
+const workspace = process.env.GITHUB_WORKSPACE
 
 const sfdxProject = JSON.parse(fs.readFileSync('sfdx-project.json', 'utf-8')) as SfdxProjectDef
 const projectDirectories = sfdxProject.packageDirectories.map(dir => dir.path)
-const currentDir = process.cwd()
 for(const projectDirectory of projectDirectories) {
-    if(fs.existsSync(`${currentDir}/${projectDirectory}`)) {
-        fs.renameSync(`${currentDir}/${projectDirectory}`, `${currentDir}/${workflowDir}/${projectDirectory}`)
+    if(fs.existsSync(`${workspace}/${projectDirectory}`)) {
+        fs.renameSync(`${workspace}/${projectDirectory}`, `${actionPath}/${projectDirectory}`)
     }
 }
